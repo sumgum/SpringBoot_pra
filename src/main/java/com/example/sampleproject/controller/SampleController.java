217329a44a1d2682beebfd9994ba3bcd1294ff8f
Sumi Gunyu <sumkakkyo@gmail.com>
2020-06-04 07:14:36 +0900

    Initial Commit

---
+++ src/main/java/com/example/sampleproject/controller/SampleController.java
@@ -0,0 +1,32 @@
+package com.example.sampleproject.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class SampleController {
+  public static void main(String[] args) {
+    try(ConfigurableApplicationContext ctx = SpringApplication.run(SampleController.class, args)) {
+      ctx.getBean(SampleController.class).sendMail();
+    }
+  }
+  @Autowired
+  private MailSender sender;
+
+  public void sendMail() {
+    SimpleMailMessage msg = new SimpleMailMessage();
+
+    msg.setFrom("test@mail.com");
+    msg.setTo("sumkakkyo@gmail.com");
+    msg.setSubject("test mail");
+    msg.setText("Spring Boot test mail");
+
+    this.sender.send(msg);
+  }
+}
